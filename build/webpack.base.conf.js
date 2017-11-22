38f45ba11b133c49747d1d61f763739b4b8f0a50
YourtionGuo <yourtion@gmail.com>
2017-11-22 10:34:23 +0800

    update project

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,7 +11,7 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: './example/main.js'
   },
   output: {
     path: config.build.assetsRoot,
