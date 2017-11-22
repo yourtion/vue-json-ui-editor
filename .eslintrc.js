b424fa32854f235d84fd416afc0bfce6d7b7b916
YourtionGuo <yourtion@gmail.com>
2017-11-22 10:31:05 +0800

    init npm

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
