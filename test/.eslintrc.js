86cc0a3b0aa5131178ba1e90f39b6f85891c8dff
YourtionGuo <yourtion@gmail.com>
2017-11-25 12:39:45 +0800

    init jest

---
+++ test/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: 'guo/mocha',
+  rules: {
+    'valid-jsdoc': 'off',
+  }
+};
---
+++ test/simple.test.js
@@ -0,0 +1,7 @@
+import JsonEditor from '../lib/json-editor.min';
+
+describe('Component', () => {
+  test('Base Test', () => {
+    console.log('test', JsonEditor.name);
+  });
+});
