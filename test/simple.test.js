ea20966c952c226558f48635b8c8c5ffa483e4b9
YourtionGuo <yourtion@gmail.com>
2017-11-26 11:29:50 +0800

    add simple test

--- test/simple.test.js
+++ test/simple.test.js
@@ -1,10 +1,13 @@
+'use strict';
+
 import { mount } from 'vue-test-utils';
+import { createRenderer } from 'vue-server-renderer';
+
 import JsonEditorLib from '../lib/json-editor.min.js';
 import JsonEditorSrc from '@/JsonEditor.vue';
-
 const JsonEditor = process.env.TEST_LIB ? JsonEditorLib : JsonEditorSrc;
 
-const schema = require('./simple.json');
+const schema = Object.freeze(require('./data/simple.json'));
 
 describe('Component', () => {
   test('Mount Test', () => {
@@ -12,5 +15,17 @@ describe('Component', () => {
       propsData: { schema },
     });
     expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test('Snapshot', done => {
+    const wrapper = mount(JsonEditor, {
+      propsData: { schema },
+    });
+    const renderer = createRenderer();
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err);
+      expect(str).toMatchSnapshot();
+      done();
+    });
   });
 });
