168cb3957980efd89b99e8e3c82bfab65a7870b6
YourtionGuo <yourtion@gmail.com>
2017-11-26 12:10:44 +0800

    add test from vue-json-schema (https://github.com/demsking/vue-json-schema/blob/master/test/specs/form-schema.spec.js)

---
+++ test/component.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import JsonEditorLib from '../lib/json-editor.min.js';
+import JsonEditorSrc from '@/JsonEditor.vue';
+
+const JsonEditor = process.env.TEST_LIB ? JsonEditorLib : JsonEditorSrc;
+
+describe('JsonEditor', () => {
+  it('should have a created hook', () => {
+    expect(typeof JsonEditor.created).toBe('function');
+  });
+
+  it('should have a mounted hook', () => {
+    expect(typeof JsonEditor.mounted).toBe('function');
+  });
+
+  it('should have a changed method', () => {
+    expect(typeof JsonEditor.methods.changed).toBe('function');
+  });
+
+  it('should have a input method', () => {
+    expect(typeof JsonEditor.methods.input).toBe('function');
+  });
+
+  it('should have a reset method', () => {
+    expect(typeof JsonEditor.methods.reset).toBe('function');
+  });
+
+  it('should have a submit method', () => {
+    expect(typeof JsonEditor.methods.submit).toBe('function');
+  });
+
+  it('should have a setErrorMessage method', () => {
+    expect(typeof JsonEditor.methods.setErrorMessage).toBe('function');
+  });
+
+  it('should have a clearErrorMessage method', () => {
+    expect(typeof JsonEditor.methods.clearErrorMessage).toBe('function');
+  });
+
+  // Evaluate the results of functions in
+  // the raw component options
+  it('should set the correct default data', () => {
+    expect(typeof JsonEditor.data).toBe('function');
+    const defaultData = JsonEditor.data();
+
+    expect(Object.keys(defaultData.default).length).toBe(0);
+    // expect(defaultData.fields.length).toBe(0);
+    expect(defaultData.error).toBe(null);
+  });
+});
---
+++ test/schema.test.js
@@ -0,0 +1,88 @@
+'use strict';
+
+import { mount } from 'vue-test-utils';
+import { createRenderer } from 'vue-server-renderer';
+
+import JsonEditorLib from '../lib/json-editor.min.js';
+import JsonEditorSrc from '@/JsonEditor.vue';
+const JsonEditor = process.env.TEST_LIB ? JsonEditorLib : JsonEditorSrc;
+
+const schema = Object.freeze(require('./data/signup.json'));
+
+describe('schema', () => {
+  const model = {};
+  const wrapper = mount(JsonEditor, {
+    propsData: { schema, model },
+  });
+  const component = wrapper.vm;
+
+  const form = component.$el.getElementsByTagName('form')[0];
+  const inputs = form.elements;
+  const button = form.getElementsByTagName('button')[0];
+
+  const attr = (input, name) => input.getAttribute(name);
+
+  for (const fieldName in schema.properties) {
+    const field = schema.properties[fieldName];
+
+    if (field.visible === false) {
+      it(`invisible input.${ fieldName } should be undefined`, () => {
+        expect(inputs[fieldName]).toBe(undefined);
+      });
+      continue;
+    }
+
+    const input = inputs[fieldName];
+
+    if (!field.attrs) {
+      field.attrs = {};
+    }
+
+    field.attrs.name = fieldName;
+
+    if (field.type === 'boolean') {
+      field.attrs.type = 'checkbox';
+    }
+
+    if (field.minLength) {
+      field.attrs.minlength = field.minLength;
+    }
+
+    if (field.maxLength) {
+      field.attrs.maxlength = field.maxLength;
+    }
+
+    if (field.required) {
+      field.attrs.required = true;
+
+      if (field.attrs.placeholder) {
+        field.attrs.placeholder += ' *';
+      }
+    }
+
+    for (const attrName in field.attrs) {
+      it(`input.${ fieldName } should have attribute '${ attrName }'`, () => {
+        expect(attr(input, attrName)).toMatch(new RegExp(`${ field.attrs[attrName] }`));
+      });
+    }
+  }
+
+  it('should have a submit button', () => {
+    expect(attr(button, 'type')).toBe('submit');
+  });
+
+  it('should have a button with Submit label', () => {
+    expect(attr(button, 'label')).toBe('Submit');
+    expect(button.innerHTML).toBe('Submit');
+  });
+
+  test('Snapshot', done => {
+    const renderer = createRenderer();
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err);
+      expect(str).toMatchSnapshot();
+      done();
+    });
+  });
+
+});
